65a9b6db30cc55747440612b52d25e032e43b08c
Lamin Lumumba <tharsis.queiroz@toodoo.com.br>
2022-11-12 00:48:20 -0300

    Finished register screen and changes in login layout

--- lib/login.dart
+++ lib/login.dart
@@ -23,113 +23,134 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.only(left: 30.0, right: 30.0,top: 60.0),
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        scrollDirection: Axis.vertical,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.only(bottom: 50.0,top: 150.0),
-              child: const Text("Welcome to login page",style: TextStyle(color: Colors.purple,fontSize: 25.0)),
-            ),
-            Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    TextFormField(
-                      controller: _emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.person_outline,color: Colors.purple),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.horizontal(
-                                left: Radius.circular(5.0),
-                                right: Radius.circular(5.0))
-                        ),
-                        label: Text("Email"),
-                        hintText: "fulano@exemplo.com",
-                        labelStyle: TextStyle(fontSize: 19.0, color: Colors.purple),
-                      ),
-                      validator: (email){
-                        if(email == null || email.isEmpty){
-                          emailValid = false;
-                          return "Enter your e-mail here.";
-                        } if (!EmailValidator.validate(email)){
-                          emailValid = false;
-                          return "Enter valid e-mail.";
-                        }
-                        emailValid = true;
-                        return null;
-                      },
-                    ),
-                    const Divider(height: 40.0,color: Colors.transparent),
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.numbers,color: Colors.purple),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.horizontal(
-                                left: Radius.circular(5.0),
-                                right: Radius.circular(5.0))
-                        ),
-                        label: Text("Password"),
-                        labelStyle: TextStyle(fontSize: 19.0,color: Colors.purple),
-                      ),
-                      validator: (password){
-                        if(password == null || password.isEmpty){
-                          passwordValid = false;
-                          return "Insert your password here";
-                        } else if( password.length < 8){
-                          passwordValid = false;
-                          return "Password has to be bigger than 8 chars";
-                        }
-                        passwordValid = true;
-                        return null;
-                      },
-                    ),
-                    const Divider(height: 40.0, color: Colors.transparent),
-                    Column(
-                      children: [
-                        ElevatedButton(onPressed: (){
-                          if(_formKey.currentState!.validate()){
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text("Logged in")),
-                            );
-                          } else {
-                            passwordValid ? false : _passwordController.clear();
-                            emailValid ? false : _emailController.clear();
-                          }
-                        },
-                          style: const ButtonStyle(backgroundColor:MaterialStatePropertyAll(Colors.purple)),
-                          child: const Text("Sign in"),
-                        ),
-                        OutlinedButton(onPressed: (){
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                              builder: (context) => const Register()
-                          ));
-                        },
-                          child: const Text("Sign up",style: TextStyle(color: Colors.purple)),
-                        ),
-                      ],
-                    )
-                  ],
-                )
-            )
-          ],
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
-      )
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 60.0),
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(bottom: 50.0, top: 150.0),
+                child: const Text("Welcome to login page",
+                    style: TextStyle(color: Colors.purple, fontSize: 25.0)),
+              ),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          icon: Icon(
+                              Icons.person_outline, color: Colors.purple),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.horizontal(
+                                  left: Radius.circular(5.0),
+                                  right: Radius.circular(5.0))
+                          ),
+                          label: Text("Email"),
+                          hintText: "fulano@exemplo.com",
+                          labelStyle: TextStyle(
+                              fontSize: 19.0, color: Colors.purple),
+                        ),
+                        validator: (email) {
+                          if (email == null || email.isEmpty) {
+                            emailValid = false;
+                            return "Enter your e-mail here.";
+                          }
+                          if (!EmailValidator.validate(email)) {
+                            emailValid = false;
+                            return "Enter valid e-mail.";
+                          }
+                          emailValid = true;
+                          return null;
+                        },
+                      ),
+                      const Divider(height: 40.0, color: Colors.transparent),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.numbers, color: Colors.purple),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.horizontal(
+                                  left: Radius.circular(5.0),
+                                  right: Radius.circular(5.0))
+                          ),
+                          label: Text("Password"),
+                          labelStyle: TextStyle(
+                              fontSize: 19.0, color: Colors.purple),
+                        ),
+                        validator: (password) {
+                          if (password == null || password.isEmpty) {
+                            passwordValid = false;
+                            return "Insert your password here";
+                          } else if (password.length < 8) {
+                            passwordValid = false;
+                            return "Password has to be bigger than 8 chars";
+                          }
+                          passwordValid = true;
+                          return null;
+                        },
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(
+                            25.0, 30.0, 25.0, 20.0),
+                        child: Column(
+                          children: [
+                            Padding(padding: const EdgeInsets.only(top: 30.0),
+                              child: ElevatedButton(onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text("Logged in")),
+                                  );
+                                } else {
+                                  passwordValid ? false : _passwordController
+                                      .clear();
+                                  emailValid ? false : _emailController.clear();
+                                }
+                              },
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.purple,
+                                    minimumSize: const Size.fromHeight(50)
+                                ),
+                                child: const Text("Sign in"),
+                              ),
+                            ),
+                             Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
+                              child: OutlinedButton(onPressed: () {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const Register()
+                                    ));
+                              },
+                                style: OutlinedButton.styleFrom(
+                                    minimumSize: const Size.fromHeight(50)
+                                ),
+                                child: const Text("Sign up", style: TextStyle(
+                                    color: Colors.purple)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+              )
+            ],
+          ),
+        )
     );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 import 'login.dart';
 
 class Register extends StatefulWidget {
@@ -13,6 +14,7 @@ class _RegisterState extends State<Register> {
 
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _passwordCheckController = TextEditingController();
   final _nameController = TextEditingController();
 
   @override
@@ -33,7 +35,8 @@ class _RegisterState extends State<Register> {
           widthFactor: 1.0,
           child: Column(
             children: <Widget>[
-              const Text("Register Page", style: TextStyle(color: Colors.purple, fontSize: 30.0)),
+              const Text("Create an account",
+                  style: TextStyle(color: Colors.purple, fontSize: 30.0)),
               Padding(
                 padding: const EdgeInsets.only(top: 70.0),
                 child: Form(
@@ -55,6 +58,12 @@ class _RegisterState extends State<Register> {
                                     left: Radius.circular(5.0),
                                     right: Radius.circular(5.0))),
                           ),
+                          validator: (name) {
+                            if (name == null || name.isEmpty) {
+                              return "Insert your name here";
+                            }
+                            return null;
+                          },
                         ),
                         Padding(
                           padding: const EdgeInsets.only(top: 24.0),
@@ -67,11 +76,21 @@ class _RegisterState extends State<Register> {
                                 color: Colors.purple,
                               ),
                               label: Text("Email"),
+                              labelStyle: TextStyle(color: Colors.purple),
                               enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.horizontal(
                                       left: Radius.circular(5.0),
                                       right: Radius.circular(5.0))),
                             ),
+                            validator: (email) {
+                              if (email == null || email.isEmpty) {
+                                return "Enter your e-mail here.";
+                              }
+                              if (!EmailValidator.validate(email)) {
+                                return "Enter valid e-mail.";
+                              }
+                              return null;
+                            },
                           ),
                         ),
                         Padding(
@@ -82,31 +101,89 @@ class _RegisterState extends State<Register> {
                             decoration: const InputDecoration(
                               icon: Icon(Icons.numbers, color: Colors.purple),
                               label: Text("Password"),
+                              labelStyle: TextStyle(color: Colors.purple),
                               enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.horizontal(
                                       left: Radius.circular(5.0),
                                       right: Radius.circular(5.0))),
                             ),
+                            validator: (password) {
+                              if (password == null || password.isEmpty) {
+                                return "Insert your password here";
+                              } else if (password.length < 8) {
+                                return "Password has to be bigger than 8 chars";
+                              }
+                              return null;
+                            },
                           ),
                         ),
                         Padding(
                           padding: const EdgeInsets.only(top: 24.0),
                           child: TextFormField(
-                            controller: _passwordController,
+                            controller: _passwordCheckController,
                             keyboardType: TextInputType.visiblePassword,
                             decoration: const InputDecoration(
                               icon: Icon(Icons.numbers, color: Colors.purple),
                               label: Text("Password confirmation"),
+                              labelStyle: TextStyle(color: Colors.purple),
                               enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.horizontal(
                                       left: Radius.circular(5.0),
                                       right: Radius.circular(5.0))),
                             ),
+                            validator: (passwordConfirm) {
+                              if (passwordConfirm == null ||
+                                  passwordConfirm.isEmpty ||
+                                  passwordConfirm != _passwordController.text) {
+                                return "Repeat first password here.";
+                              }
+                              return null;
+                            },
                           ),
                         ),
                       ],
                     )),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 25.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_registerKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("Account created")),
+                          );
+                        }
+                      },
+                      style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.purple)),
+                      child: const Text("Create an account"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Login()
+                            ),
+                        );
+                      },
+                      style: const ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.transparent),
+                      ),
+                      child: const Text("Already have an account?",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              decoration: TextDecoration.underline)),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
